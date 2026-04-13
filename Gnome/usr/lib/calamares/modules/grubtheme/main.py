2fdde41b2f91fd1443094ad12432f415fb658658
MartinKulich <kulich@stud.uniza.sk>
2026-04-13 21:08:26 +0200

    Module is loaded and it will run

---
+++ Gnome/usr/lib/calamares/modules/grubtheme/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import re
+import libcalamares
+
+
+def detect_resolution():
+
+    try:
+        result = subprocess.run(
+            ["xrandr", "--current"],
+            capture_output=True,
+            text=True,
+            check=True
+        ).stdout
+        patern = "primary\\D+(\\d+x\\d+)"
+        return re.search(patern, result).group(1)
+        
+    except Exception as e:
+        libcalamares.utils.warning(f"xrandr failed: {e}")
+
+
+
+def run():
+
+    root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
+
+    if not root_mount_point:
+        return "Root mount point not found"
+
+    libcalamares.utils.debug(
+        f"Target root: {root_mount_point}"
+    )
+
+    # --------------------
+    # Detect resolution
+    # --------------------
+
+    resolution = detect_resolution()
+
+    if resolution:
+
+        output_file = os.path.join(
+            root_mount_point,
+            "tmp",
+            "grub_res"
+        )
+
+        os.makedirs(
+            os.path.dirname(output_file),
+            exist_ok=True
+        )
+
+        with open(output_file, "w") as f:
+            f.write(resolution)
+
+        libcalamares.utils.debug(
+            f"Resolution {resolution} saved"
+        )
+
+    else:
+        libcalamares.utils.warning(
+            "Resolution detection failed"
+        )
+
+    # --------------------
+    # Install package
+    # --------------------
+
+    package_path = "/opt/ezrepo/grub-theme-Nyarch"
+
+    try:
+
+        cmd = [
+            "pacman",
+            "-U",
+            "--noconfirm",
+            f"{package_path}*.pkg.tar.zst"
+        ]
+
+        libcalamares.utils.debug(
+            f"Running: {' '.join(cmd)}"
+        )
+
+        result = libcalamares.utils.target_env_call(cmd)
+
+        if result != 0:
+            return "Failed to install grub theme package"
+
+    except Exception as e:
+        return f"Pacman failed: {e}"
+
+    return None
+
