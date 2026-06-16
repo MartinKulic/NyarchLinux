92f5917eaead2ab974b6c6ba425fc7617197ec04
MartinKulich <kulich@stud.uniza.sk>
2026-06-16 20:43:13 +0200

    Added power submenu to grub menu, correct permissions to added 39_power_menu in grub.d

--- Gnome/profiledef.sh
+++ Gnome/profiledef.sh
@@ -20,6 +20,7 @@ file_permissions=(
   ["/etc/shadow"]="0:0:0400"
   ["/etc/gshadow"]="0:0:0400"
   ["/etc/sudoers"]="0:0:0440"
+  ["/etc/grub.d/39_power_menu"]="0:0:755"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
